34b112f1c1207d80c682b045a19a14c621b519f8
Shu Kakihana <shu.kakihana@gmail.com>
2026-07-08 04:10:32 +0900

    fix: 下段の左寄せを+15→+8に縮小

--- src/layout.ts
+++ src/layout.ts
@@ -29,10 +29,10 @@ export function createDefaultLayout(): Layout {
     createDefaultButtonLayout({ x: '280', y: '74' }),
     createDefaultButtonLayout({ x: '335', y: '76' }),
     createDefaultButtonLayout({ x: '390', y: '82' }),
-    createDefaultButtonLayout({ x: '240', y: '129' }),
-    createDefaultButtonLayout({ x: '295', y: '125' }),
-    createDefaultButtonLayout({ x: '350', y: '127' }),
-    createDefaultButtonLayout({ x: '405', y: '133' }),
+    createDefaultButtonLayout({ x: '233', y: '129' }),
+    createDefaultButtonLayout({ x: '288', y: '125' }),
+    createDefaultButtonLayout({ x: '343', y: '127' }),
+    createDefaultButtonLayout({ x: '398', y: '133' }),
   ];
 
   for (let i = buttons.length; i < TOTAL_BUTTONS; i++) {
