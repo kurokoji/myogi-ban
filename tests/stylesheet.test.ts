c7e5996af10ef733ed43e8c6c77ea77514fdbdfb
Shu Kakihana <shu.kakihana@gmail.com>
2026-07-21 04:36:10 +0900

    Stop requesting legacy button images

--- tests/stylesheet.test.ts
+++ tests/stylesheet.test.ts
@@ -14,6 +14,13 @@ test("viewer stylesheet does not contain legacy image stick rules", () => {
   assert.doesNotMatch(stylesheet, /\[class="stick"\]/);
 });
 
+test("viewer stylesheet does not request legacy preset button images", () => {
+  assert.doesNotMatch(
+    stylesheet,
+    /layout\/preset\/button-(?:released|pressed)\.png/,
+  );
+});
+
 test("editor borders follow the active color scheme", () => {
   assert.doesNotMatch(editorStylesheet, /var\(--mantine-color-gray-3\)/);
   assert.match(editorStylesheet, /var\(--mantine-color-default-border\)/);
